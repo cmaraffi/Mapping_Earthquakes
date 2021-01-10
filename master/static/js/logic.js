38b41a056b8fdd7b1ae360cc5d11efd061047339
Christa Maraffi <christamaraffi@Christas-MBP.home>
2021-01-10 15:13:22 -0500

    yup

--- master/static/js/logic.js
+++ master/static/js/logic.js
@@ -1,3 +1,6 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -29,17 +32,11 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
+//d3.json
 let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
 
+//Grabbing our 
 
-// Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
-    //creating Geojson layer  
-  // We turn each feature into a marker on the map.
-  L.geoJson(data).addTo(map);
-});
 
 
 
